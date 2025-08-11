2131b09ef421d71ad8b2c43492f64140c6323e22
ANGEL V <kit27.cse07@gmail.com>
2025-08-11 20:32:21 +0530

    Add files via upload

---
+++ Day-17/Task1.js
@@ -0,0 +1,15 @@
+let name1="Angel"
+let age=25;
+
+class person{
+    constructor(name1,age){
+        this.name1=name1;
+        this.age=age
+    }
+}
+
+let obj1 =new person(name1,age)
+let obj2=new person("Buvan",25);
+
+console.log(obj1)
+console.log(obj2)
