c1d24d607d8f4cfc7bbd4cce84279a50bca61f52
ANGEL V <kit27.cse07@gmail.com>
2025-08-19 13:07:07 +0530

    Add files via upload

---
+++ Day-21/script.js
@@ -0,0 +1,14 @@
+// let para=document.getElementsByTagName("p");
+// para[1].innerHTML="<h1>eeeeeeeeeeeee</h1>"
+// para[1].textContent="wwwwww"
+// para[2].style.color="blue"
+// para[2].style.width="25"
+// para[4].classList.add("p1");
+// para[3].classList.remove("p1");
+
+// para[4].classList.remove("p1");
+
+function a() {
+    let in1 = document.getElementById("input").value;
+    alert(in1);
+}
